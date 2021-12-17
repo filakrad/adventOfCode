c7d5d8f320b541919f42e1db527124e8cf60d2b9
filakrad <filakrad@gmail.com>
2021-12-17 21:20:10 +0100

    day 16 finished

--- days/day16/code.py
+++ days/day16/code.py
@@ -1,20 +1,8 @@
 from days import parser
 from days.utilities import timer
 
-
-length_types = {0: 15,
-                1: 11}
-
-
-def packet_factory(data):
-    version, data = int(data[:3], 2), data[3:]
-    type_id, data = int(data[:3], 2), data[3:]
-    packets = []
-    if type_id == 4:
-        packets.append(LiteralPacket(version, type_id, data))
-    else:
-        packets.append(OperatorPacket(version, type_id, data))
-    return packets
+from queue import Queue
+import math
 
 
 class LiteralPacket:
@@ -26,102 +14,125 @@ class LiteralPacket:
 
     def get_value(self):
         value = ''
-        while self.data[0] == '1':
-            part_val, self.data = self.data[1:5], self.data[5:]
+        is_last = get_n_int(1, self.data) == 0
+        while not is_last:
+            part_val = get_n_str(4, self.data)
             value += part_val
-        part_val, self.data = self.data[1:5], self.data[5:]
+            is_last = get_n_int(1, self.data) == 0
+        part_val = get_n_str(4, self.data)
         value += part_val
-        return value
+        return int(value, 2)
 
 
 class OperatorPacket:
     def __init__(self, version, type_id, data):
         self.version = version
         self.type_id = type_id
-        self.length_type_id, data = int(data[:1], 2), data[1:]
-        length = length_types[self.length_type_id]
-        self.packet_length, data = int(data[:length], 2), data[length:]
-        self.data = data[:self.packet_length]
-        self.rest_data = data[self.packet_length:]
+        self.length_type_id = get_n_int(1, data)
+        self.data = data
+        self.subpackets = []
+        if self.length_type_id == 0:
+            self.resolve_length_id_0()
+        else:
+            self.resolve_length_id_1()
+
+    def resolve_length_id_0(self):
+        length = get_n_int(15, self.data)
+        sub_data_str = get_n_str(length, self.data)
+        sub_data = Queue()
+        for b in sub_data_str:
+            sub_data.put(b)
+        while sub_data.qsize() > 0:
+            sub_packet = parse_packet(sub_data)
+            self.subpackets.append(sub_packet)
+
+    def resolve_length_id_1(self):
+        sub_packets_num = get_n_int(11, self.data)
+        for _ in range(sub_packets_num):
+            sub_packet = parse_packet(self.data)
+            self.subpackets.append(sub_packet)
+
 
 def parse_packet(data):
-    version, data = int(data[:3], 2), data[3:]
-    print("version", version)
-    type_id, data = int(data[:3], 2), data[3:]
-    # print(type_id, data)
-    value = 0
-    sum_version = version
+    version = get_n_int(3, data)
+    type_id = get_n_int(3, data)
     if type_id == 4:
-        part_value, data = parse_literals(data)
-        print(part_value, data)
+        packet = LiteralPacket(version, type_id, data)
     else:
-        part_version, part_value, data = parse_operator(data)
-        sum_version += part_version
-    value += part_value
-    return sum_version, value, data
+        packet = OperatorPacket(version, type_id, data)
+    return packet
 
 
-def parse_literals(data):
-    value = ''
-    while data[0] == '1':
-        part_val, data = data[1:5], data[5:]
-        value += part_val
-    part_val, data = data[1:5], data[5:]
-    value += part_val
-    return int(value, 2), data
+def get_n_int(n, data):
+    new_str = ''
+    for _ in range(n):
+        new_str += data.get()
+    return int(new_str, 2)
 
 
-def parse_operator(data):
-    length_type_id, data = int(data[:1], 2), data[1:]
-    length = length_types[length_type_id]
-    print(length, data)
-    packet_length, data = int(data[:length], 2), data[length:]
-    print(packet_length, data)
-    sub_data, rest_data = data[:packet_length], data[packet_length:]
+def get_n_str(n, data):
+    new_str = ''
+    for _ in range(n):
+        new_str += data.get()
+    return new_str
 
-    sum_version = 0
-    value = 0
-    version = 0
-    while sub_data and int(sub_data, 2) != 0:
-        print("subpacket", sub_data)
-        version, part_value, sub_data = parse_packet(sub_data)
-        value += part_value
-    sum_version += version
-    return sum_version, value, rest_data
 
+def get_version_sum(packet):
+    version = packet.version
+    if isinstance(packet, LiteralPacket):
+        return packet.version
+    for p in packet.subpackets:
+        version += get_version_sum(p)
+    return version
+
+
+def value_decoder(packet):
+    if isinstance(packet, LiteralPacket):
+        return packet.value
+    sub_values = [value_decoder(p) for p in packet.subpackets]
+    if packet.type_id == 0:
+        return sum(sub_values)
+    elif packet.type_id == 1:
+        return math.prod(sub_values)
+    elif packet.type_id == 2:
+        return min(sub_values)
+    elif packet.type_id == 3:
+        return max(sub_values)
+    elif packet.type_id == 5:
+        return int(sub_values[0] > sub_values[1])
+    elif packet.type_id == 6:
+        return int(sub_values[0] < sub_values[1])
+    elif packet.type_id == 7:
+        return int(sub_values[0] == sub_values[1])
 
 
 @timer
 def part01(data):
-    # print(data)
-    # p = Packet(data)
-    # print(data)
-    # print(p.data)
-    version, value, _ = parse_packet(data)
-    print(version)
-
+    packet = parse_packet(data)
+    return get_version_sum(packet)
 
 
 @timer
 def part02(data):
-    pass
+    packet = parse_packet(data)
+    return value_decoder(packet)
 
 
 @timer
 def load_data():
-    str_data = parser.load_rows_to_list("test.txt", lambda line: line)[0]
-    print(str_data)
-    # int_data = [int(x, 16) for x in str_data]
-    # print(int_data)
+    str_data = parser.load_rows_to_list("day16.txt", lambda line: line)[0]
     bit_str = f'{int(str_data, 16):0>{len(str_data)*4}b}'
-    return bit_str
+    bit_queue = Queue()
+    for b in bit_str:
+        bit_queue.put(b)
+    return bit_queue
 
 
 @timer
 def main():
     data = load_data()
-    print(data)
     print(part01(data))
+    data = load_data()
     print(part02(data))
 
 
