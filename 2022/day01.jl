5f07370857a7be242d502b33567325d6e0f80ea8
filakrad <filakrad@gmail.com>
2022-12-01 08:51:30 +0100

    day01

--- 2022/day01.jl
+++ 2022/day01.jl
@@ -1,14 +1,22 @@
-
-FILE_NAME = "day01.txt"
-
 function read_file(file_name)
     arr = []
-    f = open(file_name, "r")
-        line = readline(f)
-        append!(arr, line)
-    close(f)
+    open(file_name, "r") do f
+        while ! eof(f)
+            line = readline(f)
+            partial_sum = 0
+            while ! isempty(line)
+                partial_sum += parse(Int64, line)
+                line = readline(f)
+            end
+            append!(arr, partial_sum)
+        end
+    end
     return arr
 end
 
 x = read_file("day01.txt")
-print(x)
+println(maximum(x))
+
+
+sort!(x, rev=true)
+println(sum(x[1:3]))
