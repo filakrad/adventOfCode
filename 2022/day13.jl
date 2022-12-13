26ab8a70fb805df40840504defc30519268ff9c3
filakrad <filakrad@gmail.com>
2022-12-13 19:56:12 +0100

    d13 slight readability improvement

--- 2022/day13.jl
+++ 2022/day13.jl
@@ -6,38 +6,41 @@ import JSON
 loaded = read("./day13.txt", String)
 pairs = split(loaded, "\n\n")
 
-
-function compare(left, right)
-    if typeof(left) == Int && typeof(right) == Int
-        if left == right
-            return 0
-        elseif left < right
-            return 1
-        else
-            return 2
-        end
-    elseif typeof(left) == Vector{Any} && typeof(right) == Int
-        return compare(left, [right])
-    elseif typeof(left) == Int && typeof(right) == Vector{Any}
-        return compare([left], right)
+function compare(left::Int, right::Int)
+    if left == right
+        return 0
+    elseif left < right
+        return 1
     else
-        if length(left) == 0 && length(right) == 0
-            return 0
-        elseif length(left) == 0
-            return 1
-        elseif length(right) == 0
-            return 2
+        return 2
+    end
+end
+
+function compare(left::Vector, right::Int)
+    return compare(left, [right])
+end
+
+function compare(left::Int, right::Vector)
+    return compare([left], right)
+end
+
+function compare(left::Vector, right::Vector)
+    if length(left) == 0 && length(right) == 0
+        return 0
+    elseif length(left) == 0
+        return 1
+    elseif length(right) == 0
+        return 2
+    else
+        x = compare(left[1], right[1])
+        if x > 0 
+            return x
         else
-            x = compare(left[1], right[1])
-            if x > 0 
-                return x
-            else
-                lleft = copy(left)
-                rright = copy(right)
-                popfirst!(lleft)
-                popfirst!(rright)
-                return compare(lleft, rright)
-            end
+            lleft = copy(left)
+            rright = copy(right)
+            popfirst!(lleft)
+            popfirst!(rright)
+            return compare(lleft, rright)
         end
     end
 end
@@ -46,13 +49,9 @@ end
 function part01()
     total = 0
     for (i, p) in enumerate(pairs)
-        l, r = split(p, "\n")
-        right = JSON.parse(r)
-        left = JSON.parse(l)
-        x = compare(left, right)
-        if x == 0
-            println("$left, $right")
-        elseif x == 1
+        l, r = split(p, "\n") .|> JSON.parse
+        x = compare(l, r) # 0 -> same, 1-> left is smaller, 2 -> left is bigger
+        if x == 1
             total += i
         end
     end
@@ -69,14 +68,10 @@ function part02()
     all_packets = []
     start = [[2]]
     ending = [[6]]
-    push!(all_packets, start)
-    push!(all_packets, ending)
+    push!(all_packets, start, ending)
     for p in pairs
-        l, r = split(p, "\n")
-        right = JSON.parse(r)
-        left = JSON.parse(l)
-        push!(all_packets, left)
-        push!(all_packets, right)
+        l, r = split(p, "\n") .|> JSON.parse
+        push!(all_packets, l, r)
     end
     sort!(all_packets, lt=own_isless)
 
